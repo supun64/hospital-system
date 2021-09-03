35a21f97f8ce56367c044c06eef2761d53bd30e4
poornaSavindi <prnsavindi11@gmail.com>
2021-09-03 23:53:38 +0530

    finalized version_02

--- app/views/includes/header.php
+++ app/views/includes/header.php
@@ -6,10 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo SITE_NAME ?></title>
     <link rel="stylesheet" href="<?php echo URL_ROOT;?>/public/css/styles.css">
-    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,700;1,100;1,300;1,900&display=swap" rel="stylesheet">
     <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
 </head>
 <body>
-    
--- app/views/pages/dashboard.php
+++ app/views/pages/dashboard.php
@@ -65,5 +65,37 @@
     </div>
 
 </div>
-<?php require_once URL_ROOT."/public/javascript/script.js"?>
+<script>
+    //for highlighting the selected bar
+    var side_bar_options = document.querySelectorAll('.option');
+    var link_icons = document.querySelectorAll('.link-icon-img');
+    for (let i = 0; i < side_bar_options.length; i++) {
+        side_bar_options[i].onclick = function(){
+            for (let j = 0; j < side_bar_options.length; j++) {
+                side_bar_options[j].className = "option";
+            }
+            for (let j = 0; j < link_icons.length; j++) {
+                link_icons[j].className = "link-icon-img";
+            }
+            side_bar_options[i].className= "option active";
+            link_icons[i - 1].className = "link-icon-img img-active";
+        }
+
+    }
+
+    //for minimizing the side banner
+    var side_bar = document.querySelector('.side-bar');
+    var toggle_button = document.querySelector('.toggle-icon');
+
+    toggle_button.onclick = function(){
+    let class_Name = side_bar.className;
+    if (class_Name.includes("minimized")) {
+        side_bar.className = "side-bar";
+    } else {
+        side_bar.className = "side-bar minimized";
+    }
+    }
+
+
+</script>
 <?php require_once APP_ROOT."/views/includes/footer.php"?>
