8d2d195d886f29e0baed5bec0a463f0c4a5d7d53
neth99-coder <nethmi.19@cse.mrt.ac.lk>
2021-11-14 21:49:56 +0530

    added user management

--- app/views/includes/header.php
+++ app/views/includes/header.php
@@ -13,6 +13,14 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+
+
+    <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    </head>
+
+    <!-- feather -->
+    <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
     </head>
 
 
