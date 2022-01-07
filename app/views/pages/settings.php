0329598d2b27ce66c493b63b28cb3fa377434bbf
Kumuthu <kumuthua@gmail.com>
2022-01-07 17:38:25 +0530

    Settings page corrected

--- app/views/pages/settings.php
+++ app/views/pages/settings.php
@@ -73,100 +73,85 @@ if ($_SESSION['is_admin']) {
                                     <input type="text" class="form-control" id="hospitalname" value="<?= $data['hospital_name'] ?>" disabled="" />
                                 </div>
                             </div>
-                        </div>
-                        <?php if (isset($data['error1'])) : ?>
-                            <div class="row mb-4">
-                                <div class="alert alert-danger" role="alert">
-                                    <?= $data["error1"] ?>
+                            <?php if (isset($data['error1'])) : ?>
+                                <div class="row mb-4">
+                                    <div class="alert alert-danger" role="alert">
+                                        <?= $data["error1"] ?>
+                                    </div>
                                 </div>
-                            </div>
-                        <?php endif; ?>
-                        <button class='settings-info-element btn btn-primary' type="submit">Save Changes</button>
+                            <?php endif; ?>
+                            <button class='settings-info-element btn btn-primary' type="submit">Save Changes</button>
+                    </div>
                     </form>
                 </div>
 
-                    <!-- Info area -->
+                <!-- Info area -->
 
-
-                    <div class="settings-info">
-                        <ul class="nav nav-tabs settings-sub-div-title-section mb-4" id="settings-password-ul" role="tablist">
-                            <li class="nav-item">
-                                <a class="nav-link settings-sub-div-title active" id="account-tab">Password Information</a>
-                            </li>
-                        </ul>
-                        <form class='settings-password-form' action="<?= URL_ROOT; ?>/pages/settings" method="POST">
-                            <!-- Can't change th user name -->
-                            <div class="settings-info-element form-group">
-                                <label for="username">User ID</label>
-                                <input type="text" class="form-control" id="username" value="<?= $data['user_id'] ?>" disabled="" />
+                <div class="settings-info">
+                    <ul class="nav nav-tabs mb-4" id="settings-password-ul" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" id="account-tab">Password Information</a>
+                        </li>
+                    </ul>
+                    <form class='settings-password-form' action="<?= URL_ROOT; ?>/pages/settings" method="POST">
+                        <!-- Can't change th user name -->
+                        <div class="settings-info-element form-group">
+                            <label for="username">User ID</label>
+                            <input type="text" class="form-control" id="username" value="<?= $data['user_id'] ?>" disabled="" />
+                        </div>
+                        <hr class="my-4" />
+                        <div class="row mb-4">
+                            <div class="col-md-6">
+                                <div class="settings-info-element form-group">
+                                    <label for="inputPassword4">Old Password</label>
+                                    <input type="password" class="form-control" id="old_password" name="passwords[old_password]" required minlength="8" />
+                                </div>
+                                <div class="settings-info-element form-group">
+                                    <label for="inputPassword5">New Password</label>
+                                    <input type="password" class="form-control" id="new_password" name="passwords[new_password]" required minlength="8" />
+                                    <small id="same-password" class="form-text invisible">"Your new password can not be same as your recent passwords."</small>
+                                </div>
+                                <div class="settings-info-element form-group">
+                                    <label for="inputPassword6">Confirm Password</label>
+                                    <input type="password" class="form-control" id="confirmed_password" name="passwords[confirm_password]" required minlength="8" />
+                                    <small id="matching-passwords" class="form-text invisible">"Please make sure your passwords match."</small>
+                                </div>
                             </div>
-<<<<<<< HEAD
+                            <div class="col-md-6">
+                                <p class="settings-info-element mb-2">Password requirements</p>
+                                <p class="settings-info-element small text-muted mb-2">
+                                    To create a new password, you have to meet all of the
+                                    following requirements:
+                                </p>
+                                <ul class="settings-info-element small text-muted pl-4 mb-0">
+                                    <li>Minimum 8 character</li>
+                                    <li>At least one special character</li>
+                                    <li>At least one number</li>
+                                    <li>Can’t be the same as a previous password</li>
+                                </ul>
+                            </div>
                         </div>
                         <?php if (isset($data['error2'])) : ?>
                             <div class="row mb-4">
                                 <div class="alert alert-danger" role="alert">
                                     <?= $data["error2"] ?>
-=======
-                            <hr class="my-4" />
-                            <div class="row mb-4">
-                                <div class="col-md-6">
-                                    <div class="settings-info-element form-group">
-                                        <label for="inputPassword4">Old Password</label>
-                                        <input type="password" class="form-control" id="old_password" name="passwords[old_password]" required minlength="8" />
-                                    </div>
-                                    <div class="settings-info-element form-group">
-                                        <label for="inputPassword5">New Password</label>
-                                        <input type="password" class="form-control" id="new_password" name="passwords[new_password]" required minlength="8" />
-                                        <small id="same-password" class="form-text invisible">"Your new password can not be same as your recent passwords."</small>
-                                    </div>
-                                    <div class="settings-info-element form-group">
-                                        <label for="inputPassword6">Confirm Password</label>
-                                        <input type="password" class="form-control" id="confirmed_password" name="passwords[confirm_password]" required minlength="8" />
-                                        <small id="matching-passwords" class="form-text invisible">"Please make sure your passwords match."</small>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <p class="settings-info-element mb-2">Password requirements</p>
-                                    <p class="settings-info-element small text-muted mb-2">
-                                        To create a new password, you have to meet all of the
-                                        following requirements:
-                                    </p>
-                                    <ul class="settings-info-element small text-muted pl-4 mb-0">
-                                        <li>Minimum 8 character</li>
-                                        <li>At least one special character</li>
-                                        <li>At least one number</li>
-                                        <li>Can’t be the same as a previous password</li>
-                                    </ul>
->>>>>>> 1d0a702e034ed5728c542b71676ef44cef2a02cd
                                 </div>
                             </div>
-                            <?php if (isset($data['errors'])) : ?>
-                                <div class="row mb-4">
-                                    <div class="alert alert-danger" role="alert">
-                                        <?= $data["errors"] ?>
-                                    </div>
-                                </div>
-                            <?php endif; ?>
-                            <div class="settings-info-element row mb-4">
-                                <div class="col-md-6">
-                                    <button type="submit" name='password-changed' class="btn btn-primary" onclick="return validate_password()">
-                                        Update Password
-                                    </button>
-                                </div>
+                        <?php endif; ?>
+                        <div class="settings-info-element row mb-4">
+                            <div class="col-md-6">
+                                <button type="submit" name='password-changed' class="btn btn-primary" onclick="return validate_password()">
+                                    Update Password
+                                </button>
                             </div>
-                        </form>
-                    </div>
-                    <!-- Password Changing area -->
-
+                        </div>
+                    </form>
                 </div>
-
+                <!-- Password Changing area -->
             </div>
-
-
-
         </div>
-
-    </main>
+</div>
+</main>
 </div>
 <script src="<?= URL_ROOT ?>./public/script/admin.js"></script>
 <?php require_once APP_ROOT . "/views/includes/footer.php" ?>
