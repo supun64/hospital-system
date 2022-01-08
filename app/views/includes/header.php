9981f8ec3830d2d4e1ec2b9a7dc0bf7fa1a6fe8d
supun64 <l.w.supunmadusanka64@gmail.com>
2022-01-08 22:22:53 +0530

    overall: added tabicon

--- app/views/includes/header.php
+++ app/views/includes/header.php
@@ -10,6 +10,8 @@ ob_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo SITE_NAME ?></title>
+
+    <link rel="shortcut icon" href="<?php echo URL_ROOT; ?>/public/images/SquadOption-logo.png">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo URL_ROOT; ?>/public/css/styles.css">
